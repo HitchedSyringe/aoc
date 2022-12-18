398a1922ca09b224c32142803135e6c42a33c122
HitchedSyringe <66272318+HitSyr@users.noreply.github.com>
2022-12-17 21:25:21 -0600

    [AOC 2022] Add missing license headers.

--- 2022/01/first.py
+++ 2022/01/first.py
@@ -1,5 +1,21 @@
+"""
+Solution to Part 1 of Day 1 of Advent of Code 2022.
+Copyright (C) 2022-present HitchedSyringe
 
-# Part 1 of Day 1 of Advent of Code 2022.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 
 from pathlib import Path
 
--- 2022/05/second.py
+++ 2022/05/second.py
@@ -1,7 +1,23 @@
+"""
+Solution to Part 2 of Day 5 of Advent of Code 2022.
+Copyright (C) 2022-present HitchedSyringe
 
-# Part 2 of Day 5 of Advent of Code 2022.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 # ...but this code is probably worse. I did my best for 1:05 AM brain.
-
 from pathlib import Path
 
 
